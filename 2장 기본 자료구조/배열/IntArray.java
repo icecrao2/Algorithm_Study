104fa63ed0e5ea90e1bf5fcfc2e4b989c68090bb
icecrao2 <63780449+icecrao2@users.noreply.github.com>
2021-12-02 11:05:27 +0000

    2day array

---
+++ 2장 기본 자료구조/배열/IntArray.java
@@ -0,0 +1,22 @@
+package chap01;
+import java.util.Scanner;
+
+class IntArray {
+
+
+  public static void intArray(){
+    int[] a = new a[5];
+
+    a[1] = 1; 
+    a[2] =12;
+    a[3] =1211;
+    a[4] =252;
+    a[0] =32;
+
+    for(int i = 0; i < a.length ; i++){
+      System.out.println(a[i]);
+    }
+
+  }
+  
+}
