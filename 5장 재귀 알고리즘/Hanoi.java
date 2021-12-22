53ec2347c5c1dea42e77c4ee33b6dd9745513075
문재웅 <63780449+icecrao2@users.noreply.github.com>
2021-12-22 10:18:27 +0000

    13 day study

--- 5장 재귀 알고리즘/Hanoi.java
+++ 5장 재귀 알고리즘/Hanoi.java
@@ -6,9 +6,13 @@ class Hanoi {
   //no개의 원반을 x에서 y로 옮김
   static void move(int no, int x, int y) {  
 
-    move(no-1, x, 6 - x - y);
+    if(no > 1)
+      move(no-1, x, 6 - x - y);
+  
     System.out.println(no+"번 원반을 x에서 y로 옮김");
-    move(no-1, 6 - x - y, y);
+  
+    if(no > 1)
+      move(no-1, 6 - x - y, y);
   }
 
 }
